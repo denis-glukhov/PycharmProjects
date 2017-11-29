9ae4f97ce2d3dc534ab024559d2667af2947150f
denis-glukhov <dog83@ya.ru>
2017-11-30 00:57:19 +0300

    Merge https://github.com/denis-glukhov/test
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    orts
    # the commit.

--- csv3.py
+++ csv3.py
@@ -1,27 +1,19 @@
-import csv, datetime
+import csv
 
+l= []
 
+with open("/home/denis/Downloads/csv1.csv", "r", encoding='utf8') as input_file:
+    rdr = csv.reader(input_file)
+    for rec in rdr:
+        l.append(rec)
 
-input_file = open ("/home/denis/Downloads/csv.csv", "r", encoding='utf8')
-#new_items_by_support_users
-rdr = csv.reader(input_file)
-output_file = open("/home/denis/Downloads/csv1.csv", "a")
-wrtr = csv.writer(output_file)
-for rec in rdr:
-    try:
-        rec[1] = int(rec[1]) + 555
-
-    except:
-        pass
-    if int(str(datetime.datetime.now())[-2]) > 1:
-        continue
-    print(rec)
-    now = datetime.datetime.now()
-    random = str(str(now)[-2] + str(now)[-1])[-1]
-    print(str(random))
-
-    wrtr.write(rec)
-input_file.close()
-output_file.close()
+with open("/home/denis/Downloads/csv1.csv", "a") as output_file:
+    wrtr = csv.writer(output_file)
+    for rcrd in l:
+        wrtr.writerow(rcrd)
+        if rcrd == 'name,number,text':
+            continue
+        print(rcrd)
+print (l)
 
 
