72ebbc58029937e1ad44f6ee058daf7adc721f97
denis-glukhov <dog83@ya.ru>
2017-11-30 22:32:29 +0300

    Merge https://github.com/denis-glukhov/test
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    orts
    # the commit.

--- xlrd_.py
+++ xlrd_.py
@@ -9,29 +9,30 @@ def open_file(path):
     book = xlrd.open_workbook(path)
 
     # print number of sheets
-    print (book.nsheets)
+    print ("  number of sheets is - " + str(book.nsheets))
 
     # print sheet names
-    print (book.sheet_names())
+    print ("sheet names are: " + str(book.sheet_names()))
+
 
     # get the first worksheet
     first_sheet = book.sheet_by_index(0)
 
     # read a row
-    print (first_sheet.row_values(0))
+    print ("first worksheet riw is: " + str( first_sheet.row_values(0)))
 
     # read a cell
     cell = first_sheet.cell(0, 0)
-    print (cell)
-    print (cell.value)
+    print ('cell is: ', cell)
+    print ('cell value is: ', cell.value)
 
     # read a row slice
-    print (first_sheet.row_slice(rowx=0,
+    print ('row slice is ', first_sheet.row_slice(rowx=1,
                           start_colx=0,
-                          end_colx=2)
+                          end_colx=20)
            )
-
-
+"""
+"""
 # ----------------------------------------------------------------------
 if __name__ == "__main__":
     path = "test.xls"
