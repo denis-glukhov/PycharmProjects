7e7068a8097896c7d4825f6f62631862aa1708d3
denis-glukhov <dog83@ya.ru>
2018-02-15 22:48:09 +0300

    Merge https://github.com/denis-glukhov/test
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    orts
    # the commit.

--- kivy/main.py
+++ kivy/main.py
@@ -1,1 +1,19 @@
 from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.codeinput import CodeInput
+
+class MyApp (App):
+    def build(self):
+        return Button(text = 'Это КНОПКА!!!',
+                      font_size = 50,
+                      on_press = self.btn_press,
+                      background_color = [0.1, 0.9, 0.5, 1],
+                      background_normal = ''
+                      )
+    def btn_press(self, instance):
+        print("Кнопка нажата")
+        instance.text = 'Hello User!'
+
+
+if __name__ == '__main__':
+    MyApp().run()
