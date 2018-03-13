d8f9a4c201c7cabc7a6e36b78d47ea4fbb21f397
denis-glukhov <dog83@ya.ru>
2018-03-13 23:02:02 +0300

    Merge https://github.com/denis-glukhov/test
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    orts
    # the commit.

--- TRY_MATRIX_FACEBOOK_TEST.py
+++ TRY_MATRIX_FACEBOOK_TEST.py
@@ -1,22 +1,25 @@
 n=int(input())
 r=range(n)
 
-rr=range(0,n,-1)
+
 m = [[0 for j in r] for j in r]
 
 x, y = 0, n-1
-
+rr=range(y, x-1,-1)
 m[0][0]=1
 for i in r:
     for j in r:
         if i == x and j<=y and j>x:
             m[i][j]+=m[i][j-1]+1
-        elif j == y and j>=x:
+        elif j == y and j>x:
             m[i][j]+=m[i-1][j]+1
+for i in rr:
+  for j in rr:
+        if i == y and j<y:
+            m[i][j]+=m[i][j+1]+1
+        elif j == x and i < y  and i > x:
+            m[i][j] += m[i+1][j] + 1
 
-for i in (rr):
-  print (int(i))
 
-
-#for i in m:
-#  print(i)
+for i in m:
+  print(i)
